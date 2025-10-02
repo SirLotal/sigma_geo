036bed0d7c60fa3d758ab39508c660f4b11e40a3
Leonid <sigmageo.main@gmail.com>
2025-10-02 14:24:06 +0300

    project test nested+incorrect

--- backend/controllers/projects_test.go
+++ backend/controllers/projects_test.go
@@ -58,5 +58,58 @@ func TestCDVariant(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	pm.DeleteSubproject(project_name, subproject_name)
+	pm.DeleteProject(project_name)
+}
+
+func TestNestedDeletion(t *testing.T) {
+	project_name := "RMO"
+	subproject_name := "rmo_2025"
+	variant_name := "rmo_mathbattle"
+	pm := controllers.NewProjectManager()
+	pm.CreateProject(project_name)
+	pm.OpenProject(project_name)
+	pm.CreateSubproject(project_name, subproject_name)
+	pm.CreateVariant(project_name, subproject_name, variant_name)
+
+	err := pm.DeleteProject(project_name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pm.CreateProject(project_name)
+	pm.OpenProject(project_name)
+	pm.CreateSubproject(project_name, subproject_name)
+	pm.CreateVariant(project_name, subproject_name, variant_name)
+
+	err = pm.DeleteSubproject(project_name, subproject_name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pm.DeleteProject(project_name)
+}
+
+func TestIncorrectNames(t *testing.T) {
+	project_name := "RMO"
+	subproject_name := "rmo_2025"
+	incorrect_name := "..."
+	pm := controllers.NewProjectManager()
+
+	err := pm.CreateProject(incorrect_name)
+	if err == nil {
+		t.Fatal(err)
+	}
+
+	pm.CreateProject(project_name)
+	err = pm.CreateSubproject(project_name, incorrect_name)
+	if err == nil {
+		t.Fatal(err)
+	}
+
+	pm.CreateSubproject(project_name, subproject_name)
+	err = pm.CreateVariant(project_name, subproject_name, incorrect_name)
+	if err == nil {
+		t.Fatal(err)
+	}
 	pm.DeleteProject(project_name)
 }
